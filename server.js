5d7645f280a8d24c6e8aa3ef984f1ed4ef1f92cd
graemeelliott <graeme.elliott@dimedout.com>
2020-01-04 18:36:39 +0000

    Added sessions

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const dotenv = require('dotenv'),
-      mongodb = require('mongodb');
+      mongodb = require('mongodb').MongoClient;
 
 dotenv.config({ path: './config.env' });
 
 mongodb.connect(process.env.DATABASE, 
   {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
-    module.exports = client.db();
+    module.exports = client.db('eventlistings');
     const app = require('./app');
     app.listen(process.env.PORT);
 });
